f270083e14e13f0ef29b2eeb45fe3e1b1eeb2566
Keerthi <keerthi.rvk.0505@gmail.com>
2022-02-18 23:24:37 +0530

    3rd Program Added

---
+++ Program3/src/mypackage/MyClass.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		int num=9;
+		if (num % 2 == 0)
+		{
+			System.out.println("It is a even number");
+			
+		}
+		else
+			System.out.println("It is a Odd Number");
+	}
+	
+
+}
