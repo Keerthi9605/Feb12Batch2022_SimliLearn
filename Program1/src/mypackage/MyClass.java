0efd8776f6f7b23bb9b7dae4a14807af60847d00
Keerthi <keerthi.rvk.0505@gmail.com>
2022-02-18 17:43:26 +0530

    Project1 Check-in

---
+++ Program1/src/mypackage/MyClass.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0; i<5; i++)
+		
+			System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
